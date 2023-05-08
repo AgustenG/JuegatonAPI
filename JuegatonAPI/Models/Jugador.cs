00d6e6f2406eab2b53210ab657abd3e462d343a1
Aitor Martínez <aitor.martinez.7e5@itb.cat>
2023-05-08 06:25:17 +0200

    Hide password

--- JuegatonAPI/Models/Jugador.cs
+++ JuegatonAPI/Models/Jugador.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.Numerics;
+using System.Text.Json.Serialization;
 
 namespace JuegatonAPI.Models
 {
@@ -9,6 +10,7 @@ namespace JuegatonAPI.Models
         public string Apellidos { get; set; }
         [Key]
         public  string Nickname { get; set; }
+        [JsonIgnore]
         public string? Password { get; set; }
         public string Pais { get; set; }
         public string Color { get; set; }
